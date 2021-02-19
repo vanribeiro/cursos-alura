07b46d23bcd4ef24eb27f35d85d1f495bef9c9fc
vanribeiro <vanribeiro.dev@gmail.com>
2021-02-19 03:24:42 -0300

    start js scripts

---
+++ 00 - Vetores e Animação com SVG CSS JS - Allura/my-code/aula-04/assets/js/scripts.js
@@ -0,0 +1,6 @@
+import { Cards } from './Cards.js';
+
+window.onload = () => {
+    let animaCards = new Cards();
+    document.addEventListener('scroll', animaCards.scrollCards.bind(animaCards));
+}
