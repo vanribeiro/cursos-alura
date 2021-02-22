3b688cb895702b9c6de1e4ebb47c311a66ecbc3b
vanribeiro <vanribeiro.dev@gmail.com>
2021-02-21 22:34:19 -0300

    finish class 4

--- 00 - Vetores e Animação com SVG CSS JS - Allura/my-code/aula-04/assets/js/Cards.js
+++ 00 - Vetores e Animação com SVG CSS JS - Allura/my-code/aula-04/assets/js/Cards.js
@@ -11,7 +11,6 @@ class Cards {
 
     calculoScroll(){
         const posicao = this.posicaoSectionDicas.getBoundingClientRect()['y'];
-        console.log(posicao)
         if(posicao >= 25){
             this.cardEsq.style.transform = `translate(${(-posicao + (25))/10}%)`;
             this.cardDir.style.transform = `translate(${(posicao - (25))/10}%)`;
