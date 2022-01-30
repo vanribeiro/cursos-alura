e65d6c252f7a4e58c09d4886a24f4a46f1d33b35
vanribeiro <vanribeiro.dev@gmail.com>
2022-01-30 03:08:36 -0300

    Aula 05 e finalização do curso

--- 05 - Formação React/01 - Programar com JS Orientado a Objetos/02 - JS - Interfaces e Herança em Orientação a Objetos/SistemaDeAutenticacao.js
+++ 05 - Formação React/01 - Programar com JS Orientado a Objetos/02 - JS - Interfaces e Herança em Orientação a Objetos/SistemaDeAutenticacao.js
@@ -1,9 +1,17 @@
 /**
  * Ser autenticável significa ter o metodo autenticar
+ * Duck typing - "If it walks like a duck and it quacks like a duck, then it must be a duck"
  */
 class SistemaDeAutenticacao{
     static login(autenticavel, senha){
-        return autenticavel.autenticar(senha);
+        if (SistemaDeAutenticacao.ehAutenticavel(autenticavel)) {
+            return autenticavel.autenticar(senha);
+        }
+        return false;
+    }
+
+    static ehAutenticavel(autenticavel){
+        return "autenticar" in autenticavel && autenticavel.autenticar instanceof Function;
     }
 }
 
