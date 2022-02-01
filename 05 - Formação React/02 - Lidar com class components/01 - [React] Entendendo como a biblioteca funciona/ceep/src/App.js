ecac69212a4af4f0878460a329a10245b8aba477
vanribeiro <vanribeiro.dev@gmail.com>
2022-01-31 22:38:55 -0300

    Aula 05 e curso finalizado: [React] Entendendo como a biblioteca funciona.

--- 05 - Formação React/02 - Lidar com class components/01 - [React] Entendendo como a biblioteca funciona/ceep/src/App.js
+++ 05 - Formação React/02 - Lidar com class components/01 - [React] Entendendo como a biblioteca funciona/ceep/src/App.js
@@ -3,11 +3,25 @@ import FormularioCadastro from "./components/FormularioCadastro";
 import ListaDeNotas from './components/ListaDeNotas';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      notas: []
+    };
+  }
+
+  criarNota(titulo, texto){
+    const novaNota = {titulo, texto};
+    const novoArrayDeNotas = [...this.state.notas, novaNota];
+    const novoEstado =  { notas: novoArrayDeNotas}
+    this.setState(novoEstado);
+  }
+
   render(){
     return (
-      <section>
-        <FormularioCadastro />
-        <ListaDeNotas />
+      <section className='conteudo'>
+        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+        <ListaDeNotas notas={this.state.notas}/>
       </section>
     );
   }
