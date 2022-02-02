b4e727142510e3f05ad40f2b08cbecd7061a0f99
vanribeiro <vanribeiro.dev@gmail.com>
2022-02-02 01:17:53 -0300

    Aula 01 finalizada: curso [React: Ciclo de vida dos componentes].

--- 05 - Formação React/02 - Lidar com class components/02 - [React] Ciclo de vida dos componentes/ceep/src/App.js
+++ 05 - Formação React/02 - Lidar com class components/02 - [React] Ciclo de vida dos componentes/ceep/src/App.js
@@ -17,11 +17,20 @@ class App extends Component {
     this.setState(novoEstado);
   }
 
+  deletarNota(index){
+    let arrayDeNotas = this.state.notas;
+    arrayDeNotas.splice(index, 1);
+    this.setState({nota: arrayDeNotas});
+  }
+
   render(){
     return (
       <section className='conteudo'>
         <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
-        <ListaDeNotas notas={this.state.notas}/>
+        <ListaDeNotas 
+          notas={this.state.notas}
+          apagarNota={this.deletarNota.bind(this)}
+        />
       </section>
     );
   }
