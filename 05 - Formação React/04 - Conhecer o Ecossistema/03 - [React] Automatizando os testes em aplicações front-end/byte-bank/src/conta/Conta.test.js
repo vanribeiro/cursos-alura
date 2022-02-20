1a4d8f8405baa7f23cddf9ca71148356138b3ccd
vanribeiro <vanribeiro.dev@gmail.com>
2022-02-19 21:53:19 -0300

    Aulas 04, 05 e finalização do curso [React] Automatizando os testes em aplicações front-end

---
+++ 05 - Formação React/04 - Conhecer o Ecossistema/03 - [React] Automatizando os testes em aplicações front-end/byte-bank/src/conta/Conta.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {fireEvent, render, screen} from '@testing-library/react';
+import Conta from './Conta';
+
+describe('Componente de conta', () => {
+    it('Exibir o saldo da conta com formatação monetária', () => {
+        render(<Conta saldo={1000}/>)
+        const saldo = screen.getByTestId('saldo-conta');
+        expect(saldo.textContent).toBe('R$ 1000');
+    });
+
+    it('Chama a função de realiza a transação, quando o botão é clicado', () => {
+        const funcaoRealizarTransacao = jest.fn()
+        render(<Conta saldo={1000} realizarTransacao={funcaoRealizarTransacao}/>);
+        fireEvent.click(screen.getByText('Realizar operação'));
+        expect(funcaoRealizarTransacao).toHaveBeenCalled();
+    })
+});
