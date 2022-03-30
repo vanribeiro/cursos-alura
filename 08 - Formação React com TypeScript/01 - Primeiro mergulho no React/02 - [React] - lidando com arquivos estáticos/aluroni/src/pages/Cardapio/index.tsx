2e131a6b466274b04fa82074d35203e8e37a7b01
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2022-03-30 02:59:27 -0300

    Aula 03 finalizada. Aula 04 iiniciada - React: lidando com arquivos estáticos.

--- 08 - Formação React com TypeScript/01 - Primeiro mergulho no React/02 - [React] - lidando com arquivos estáticos/aluroni/src/pages/Cardapio/index.tsx
+++ 08 - Formação React com TypeScript/01 - Primeiro mergulho no React/02 - [React] - lidando com arquivos estáticos/aluroni/src/pages/Cardapio/index.tsx
@@ -1,10 +1,31 @@
 import style from './Cardapio.module.scss';
 import { ReactComponent as Logo} from 'assets/img/logo-aluroni.svg';
+import Buscador from './Buscador';
+import { useState } from 'react';
+import Filtros from './Filtros';
 
 const Cardapio = () => {
+    const [ busca, setBusca ] = useState("");
+
     return (
-        <main className={style.menu}>
-            <Logo />
+        <main>
+            <nav className={style.menu}>
+                <Logo />
+            </nav>
+            <header className={style.header}>
+                <div className={style.header__text}>
+                    A casa do código e da massa
+                </div>
+            </header>
+            <section className={style.cardapio}>
+                <h3  className={style.cardapio__titulo}>
+                    Cardápio
+                </h3>
+                <Buscador busca={busca} setBusca={setBusca}/>
+                <div className={style.cardapio__filtros}>
+                    <Filtros />
+                </div>
+            </section>
         </main>
     )
 }
