bd8cabe478d9668a0fda6b8f1e12b7af52571067
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2022-11-23 15:34:49 -0300

    Curso Performance: Aulas de 01 a 03

--- 08 - Formação React com TypeScript/05 - Cuide da performance em React/01 - [React] otimizando a performance/aluroni/src/pages/Cardapio/Itens/Item/index.tsx
+++ 08 - Formação React com TypeScript/05 - Cuide da performance em React/01 - [React] otimizando a performance/aluroni/src/pages/Cardapio/Itens/Item/index.tsx
@@ -2,10 +2,12 @@ import styles from './Item.module.scss';
 import { Prato } from 'types/Prato';
 import TagsPrato from 'components/TagsPrato';
 import { useNavigate } from 'react-router-dom';
+import { memo } from 'react';
 
-export default function Item(props: Prato) {
+function Item(props: Prato) {
   const { id, title, description, photo } = props;
   const navigate = useNavigate();
+  id === 1 && console.log(title);
 
   return (
     <div className={styles.item} onClick={() => navigate(`/prato/${id}`)}>
@@ -22,3 +24,18 @@ export default function Item(props: Prato) {
     </div>
   );
 }
+/**
+ * Usos da Função memo(<Componente />)
+ * 
+ * - Listas
+ * - Filtros
+ * - Renderização condicional
+ * 
+ * - Não deve ser usado em qualquer componente
+ * - Seu objetivo é melhorar performance e não a de previnir renderizações, 
+ *   pois pode levar a bugs, segundo a documentação
+ * 
+ */
+
+export default memo(Item);
+// export default Item;
