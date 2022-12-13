e1452bb20d6acf9e6ceb7e30456cd762b3249d9c
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2022-12-13 19:14:46 -0300

    fix: Curso: Criando sua primeira biblioteca - Aula 02

--- 10 - NodeJS - criando sua primeira biblioteca/index.js
+++ 10 - NodeJS - criando sua primeira biblioteca/index.js
@@ -8,8 +8,8 @@ function trataErro(erro) {
 // Async: async/await
 async function pegaArquivo(caminhoDoArquivo) {
     try {
-        const enconding = 'utf-8';
-        const texto = await fs.promises.readFile(caminhoDoArquivo, enconding);
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
         console.log(chalk.green(texto));
     } catch (erro) {
         trataErro(erro);
@@ -17,11 +17,13 @@ async function pegaArquivo(caminhoDoArquivo) {
         console.log(chalk.yellow('operação concluída'));
     }
 }
+pegaArquivo('./arquivos/texto.md');
+
 
 /* Async: then()
 function pegaArquivo(caminhoDoArquivo) {
-    const enconding = 'utf-8';
-    fs.promises.readFile(caminhoDoArquivo, enconding)
+    const encoding = 'utf-8';
+    fs.promises.readFile(caminhoDoArquivo, encoding)
         .then((texto) => console.log(chalk.green(texto)))
         .catch((erro) => trataErro(erro));
 }
@@ -30,10 +32,10 @@ function pegaArquivo(caminhoDoArquivo) {
 /* Sync
 function pegaArquivo(caminhoDoArquivo) {
 
-    const enconding = 'utf-8';
+    const encoding = 'utf-8';
     fs.readFile(
         caminhoDoArquivo, 
-        enconding, 
+        encoding, 
         (erro, texto) => {
             if(erro) trataErro(erro);
             console.log(chalk.green(texto));
@@ -42,4 +44,3 @@ function pegaArquivo(caminhoDoArquivo) {
 }
 */
 
-pegaArquivo('./arquivos/texto.md');
