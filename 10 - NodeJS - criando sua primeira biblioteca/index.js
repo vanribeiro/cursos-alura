36a8212d582c5e56a9ba317ca9e9ccf1a5300bd5
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2022-12-13 12:18:51 -0300

    Curso: Criando sua primeira biblioteca - Aula 01

---
+++ 10 - NodeJS - criando sua primeira biblioteca/index.js
@@ -0,0 +1,11 @@
+import chalk from "chalk";
+
+console.log(chalk.blue('olá mundo'));
+console.log('olá mundo');
+console.log('olá mundo');
+
+console.log(`
+CPU: ${chalk.red('90%')}
+RAM: ${chalk.green('40%')}
+DISK: ${chalk.yellow('70%')}
+`);
