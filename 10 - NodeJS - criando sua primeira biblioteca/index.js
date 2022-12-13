150bc63449f18c59f54855038aff8df10cb82a16
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2022-12-13 20:23:09 -0300

    Curso: Criando sua primeira biblioteca - Aula 03

--- 10 - NodeJS - criando sua primeira biblioteca/index.js
+++ 10 - NodeJS - criando sua primeira biblioteca/index.js
@@ -1,6 +1,13 @@
 import fs from 'fs';
 import chalk from "chalk";
 
+function extraiLinks(texto) {
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.]*.[^\s]*)\)/gm;
+    const capturas = [...texto.matchAll(regex)];
+    const resultados = capturas.map(capitura => ({[capitura[1]]: capitura[2]}));
+    return resultados;
+}
+
 function trataErro(erro) {
     throw new Error(chalk.red(erro.code, '- não há arquivo no diretório'));
 }
@@ -10,37 +17,12 @@ async function pegaArquivo(caminhoDoArquivo) {
     try {
         const encoding = 'utf-8';
         const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
-        console.log(chalk.green(texto));
+        console.log(extraiLinks(texto));
     } catch (erro) {
         trataErro(erro);
     } finally {
         console.log(chalk.yellow('operação concluída'));
     }
 }
+
 pegaArquivo('./arquivos/texto.md');
-
-
-/* Async: then()
-function pegaArquivo(caminhoDoArquivo) {
-    const encoding = 'utf-8';
-    fs.promises.readFile(caminhoDoArquivo, encoding)
-        .then((texto) => console.log(chalk.green(texto)))
-        .catch((erro) => trataErro(erro));
-}
-*/
-
-/* Sync
-function pegaArquivo(caminhoDoArquivo) {
-
-    const encoding = 'utf-8';
-    fs.readFile(
-        caminhoDoArquivo, 
-        encoding, 
-        (erro, texto) => {
-            if(erro) trataErro(erro);
-            console.log(chalk.green(texto));
-    });
-    
-}
-*/
-
