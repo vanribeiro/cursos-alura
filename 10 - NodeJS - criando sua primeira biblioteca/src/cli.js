67ed5f8486168c54be6b575f60b579a938cd0c0b
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2022-12-15 23:42:53 -0300

    Curso: Criando sua primeira biblioteca - Aula 04 - parte final

--- 10 - NodeJS - criando sua primeira biblioteca/src/cli.js
+++ 10 - NodeJS - criando sua primeira biblioteca/src/cli.js
@@ -1,9 +1,41 @@
 import chalk from 'chalk';
 import pegaArquivo from "./index.js";
+import fs from 'fs';
 
-async function processaTexto(caminho) {
-    const resultado = await pegaArquivo(caminho[2]);
-    console.log(chalk.yellow('lista de links:'), resultado);
+function imprimeLista(resultado, identificador = '') {
+    console.log(
+        chalk.yellow(`lista de links`),
+        chalk.black.bgCyan(`${identificador}`), resultado
+    );
+}
+
+async function processaTexto(argumentos) {
+
+    const caminho = argumentos[2];
+
+    try {
+        fs.lstatSync(caminho);
+    } catch (erro) {
+        if(erro.code === 'ENOENT'){
+            console.log(chalk.red('arquivo ou diretório não existe'));
+            return;
+        }
+    }
+
+    if(fs.lstatSync(caminho).isFile()){
+
+        const resultado = await pegaArquivo(caminho);
+        imprimeLista(resultado);
+
+    } else if(fs.lstatSync(caminho).isDirectory()){
+        const arquivos = await fs.promises.readdir(caminho);
+
+        arquivos.forEach(async(arquivo) => {
+            const lista = await pegaArquivo(`${caminho}/${arquivo}`);
+            imprimeLista(lista, arquivo);
+        });
+    }
+    
 }
 
 const caminho = process.argv;
