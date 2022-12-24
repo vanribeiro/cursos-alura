634ce631b973c04578f208fd2e6d5bb0d4eb4288
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2022-12-23 23:40:58 -0300

    Curso ORM com NodeJS I - Aulas 06, curso finalizado

--- 11 - Formação NodeJS com Express/02 - 0RM com Sequelize/01 - ORM com NodeJS - API com Sequelize e MySQL/api/controllers/PessoaController.js
+++ 11 - Formação NodeJS com Express/02 - 0RM com Sequelize/01 - ORM com NodeJS - API com Sequelize e MySQL/api/controllers/PessoaController.js
@@ -1,55 +1,131 @@
-const database = require('../models');
+const database = require("../models");
 
 class PessoaController {
+	static async pegarTodasAsPessoas(___, res) {
+		try {
+			const todasAsPessoas = await database.Pessoas.findAll();
+			return res.status(200).json(todasAsPessoas);
+		} catch (error) {
+			return res.status(500).json(error.message);
+		}
+	}
 
-    static async pegasTodasAsPessoas(___, res) {
-        try {            
-            const todasAsPessoas = await database.Pessoas.findAll();
-            return res.status(200).json(todasAsPessoas);
-        } catch (error) {
-            return res.status(500).json(error.message);
-        }
-    }
+	static async pegarPessoaPorId(req, res) {
+		const { id } = req.params;
+		try {
+			const pessoa = await database.Pessoas.findOne({
+				where: { id: Number(id) },
+			});
+			return res.status(200).json(pessoa);
+		} catch (error) {
+			return res.status(500).json(error.message);
+		}
+	}
 
-    static async pegarPessoaPorId (req, res) {
-        const { id }  = req.params;
-        try {
-            const pessoa = await database.Pessoas.findOne({ where: { id: Number(id) }});
-            return res.status(200).json(pessoa);
-        } catch (error) {
-            return res.status(500).json(error.message);
-        }
-    }
+	static async deletarPessoa(req, res) {
+		const { id } = req.params;
+		try {
+			await database.Pessoas.destroy({ where: { id: Number(id) } });
+			return res.status(200).json({ message: `${id} deletado` });
+		} catch (error) {
+			return res.status(500).json(error.message);
+		}
+	}
 
-    static async deletarPessoa (req, res) {
-        const { id }  = req.params;
-        try {
-            await database.Pessoas.destroy({ where: { id: Number(id) } });
-            return res.status(200).json({message: `${id} deletado`});
-        } catch (error) {
-            return res.status(500).json(error.message);
-        }
-    }
+	static async cadastrarPessoa(req, res) {
+		const corpoDaRequisicao = req.body;
+		try {
+			const pessoa = await database.Pessoas.create(corpoDaRequisicao);
+			return res.status(200).json(pessoa);
+		} catch (error) {
+			return res.status(500).json(error.message);
+		}
+	}
 
-    static async cadastrarPessoa (req, res) {
-        try {
-            const pessoa = await database.Pessoas.create(req.body);
-            return res.status(200).json(pessoa);
-        } catch (error) {
-            return res.status(500).json(error.message);
-        }
-    }
+	static async atualizarPessoa(req, res) {
+		const corpoDaRequisicao = req.body;
+		const { id } = req.params;
+		try {
+			await database.Pessoas.update(corpoDaRequisicao, {
+				where: { id: Number(id) },
+			});
+			const pessoaAtualizada = await database.Pessoas.findOne({
+				where: { id: Number(id) },
+			});
+			return res.status(200).send(pessoaAtualizada);
+		} catch (error) {
+			return res.status(500).json(error.message);
+		}
+	}
 
-    static async atualizarPessoa (req, res) {
-        const { id } = req.params;
-        try {
-            await database.Pessoas.update(req.body, { where: {id: Number(id)}});
-            const pessoaAtualizada = await database.Pessoas.findOne({where: { id: Number(id) }});
-            return res.status(200).send(pessoaAtualizada);
-        } catch (error) {
-            return res.status(500).json(error.message);
-        }
-    }
+	// endpoint: /pessoas/1/matricula/5
+	// endpoint: /pessoas/:estudanteId/matricula/:matriculaId
+	static async pegarMatriculaPorId(req, res) {
+		const { estudanteId, matriculaId } = req.params;
+		try {
+			const matricula = await database.Matriculas.findOne({
+				where: {
+					id: Number(matriculaId),
+					estudante_id: Number(estudanteId),
+				},
+			});
+			return res.status(200).json(matricula);
+		} catch (error) {
+			return res.status(500).json(error.message);
+		}
+	}
 
+    // endpoint: /pessoas/1/matricula
+	// endpoint: /pessoas/:estudanteId/matricula
+    static async criaUmaMatricula(req, res) {
+        const { estudanteId } = req.params;
+		const novaMatricula = { ...req.body, estudante_id: Number(estudanteId)};
+		try {
+			const novaMatriculaCriada = await database.Matriculas.create(novaMatricula);
+			return res.status(200).json(novaMatriculaCriada);
+		} catch (error) {
+			return res.status(500).json(error.message);
+		}
+	}
+
+	// endpoint: /pessoas/1/matricula/5
+	// endpoint: /pessoas/:estudanteId/matricula/:matriculaId
+    static async deletarMatricula(req, res) {
+		const { estudanteId, matriculaId } = req.params;
+		try {
+			await database.Matriculas.destroy({ 
+                where: {
+					id: Number(matriculaId),
+					estudante_id: Number(estudanteId),
+				},
+             });
+			return res.status(200).json({ message: `matricula de id ${matriculaId} foi deletada` });
+		} catch (error) {
+			return res.status(500).json(error.message);
+		}
+	}
+	// endpoint: /pessoas/1/matricula/8
+	// endpoint: /pessoas/:estudanteId/matricula/:matriculaId
+    static async atualizarMatricula(req, res) {
+		const { estudanteId, matriculaId } = req.params;
+		const corpoDaRequisicao = req.body;
+		try {
+			await database.Matriculas.update(corpoDaRequisicao, {
+				where: { 
+                    id: Number(matriculaId),
+                    estudante_id: Number(estudanteId)
+                },
+			});
+			const matriculaAtualizada = await database.Matriculas.findOne({
+				where: { 
+                    id: Number(matriculaId),
+                },
+			});
+			return res.status(200).send(matriculaAtualizada);
+		} catch (error) {
+			return res.status(500).json(error.message);
+		}
+	}
 }
+
 module.exports = PessoaController;
--- 11 - Formação NodeJS com Express/02 - 0RM com Sequelize/01 - ORM com NodeJS - API com Sequelize e MySQL/api/routes/index.js
+++ 11 - Formação NodeJS com Express/02 - 0RM com Sequelize/01 - ORM com NodeJS - API com Sequelize e MySQL/api/routes/index.js
@@ -1,10 +1,14 @@
 const bodyParser = require('body-parser');
 const pessoas = require('./pessoasRoutes.js');
+const niveis = require('./niveisRoutes.js');
+const turmas = require('./turmasRoutes.js');
 
 module.exports = app => {
     app.use(bodyParser.json());
     app.use(
-        pessoas
+        pessoas,
+        niveis,
+        turmas
     );
 }
 
