b9f5fc27045252d36ea8e73acc6e617489e41112
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2023-01-20 02:06:06 -0300

    Curso de webpack - Aulas 01 e 02

---
+++ 13 - Automação e Perfomance/01 - Webpack/webapp/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); 
+
+module.exports = {
+    entry: './app/src/js/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'app/dist'),
+        clean: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ 
+            template: './app/src/app.html',
+            filename: 'app.html',
+            hash: true
+        }),
+    ]
+};
