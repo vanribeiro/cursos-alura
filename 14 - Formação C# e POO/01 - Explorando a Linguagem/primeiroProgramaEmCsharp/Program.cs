1333268ffbd140e5a0d3a6fd811efd480f54e91a
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2023-01-22 02:59:09 -0300

    Curso 01 de C# e POO - Aula 01 e 02

---
+++ 14 - Formação C# e POO/01 - Explorando a Linguagem/primeiroProgramaEmCsharp/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Programa {
+
+    static void Main(string[] args){
+
+        Console.WriteLine("Olá!");
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+        
+    }
+}
