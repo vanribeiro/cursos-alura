f8c5282f86db22751e2919fc8a15c467a5b168af
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2023-01-22 02:58:44 -0300

    Curso 01 de C# e POO - Aula 01 e 02

---
+++ 14 - Formação C# e POO/01 - Explorando a Linguagem/CriandoVariaveis/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class Programa {
+
+    static void Main(string[] args){
+        int idade = 27;
+        double salario = 1283.18;
+        double divisao = 7.0 / 4;
+
+        short z = 15000;
+        int x = 100000;
+        long y = 2000000000000000000;
+
+        float altura = 1.52f;
+
+        //casting
+        double nota01 = 8.5;
+        int nota02 = (int)nota01;
+
+
+        Console.WriteLine("[idade]: " + idade);
+        Console.WriteLine("[salario]: " + salario);
+        Console.WriteLine("[divisao]: " + divisao);
+        Console.WriteLine("[nota02]: " + nota02);
+        Console.WriteLine("[z]: " + z);
+        Console.WriteLine("[x]: " + x);
+        Console.WriteLine("[y]: " + y);
+        Console.WriteLine("[altura]: " + altura);
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+        
+    }
+}
