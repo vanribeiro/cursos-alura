69117d16b2edaf8eb7a8be26fe77545a1fb025cc
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2023-01-22 21:20:21 -0300

    Curso 01 de C# e POO - Aula 04, 05, 06. Curso finalizado.

---
+++ 14 - Formação C# e POO/01 - Explorando a Linguagem/Condicionais/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Programa {
+
+    static void Main(string[] args){
+        int idade = 16;
+        int quantidadeDePessoas = 2;
+
+        if(idade >= 18){
+            Console.WriteLine("Pode entrar!");
+        } else{
+            if(quantidadeDePessoas >= 1){
+                Console.WriteLine("Pode entrar!");
+            } else{
+                Console.WriteLine("Não pode entrar ");
+            }
+        }
+
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+        
+    }
+}
