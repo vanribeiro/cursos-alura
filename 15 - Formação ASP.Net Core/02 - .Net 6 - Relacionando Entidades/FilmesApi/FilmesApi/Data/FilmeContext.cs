5ae97f1486eb0d0a8427f66577e5582919049f35
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2023-02-21 23:55:17 -0300

    Curso 02 de ASP.Net Core - Arquivos iniciais

---
+++ 15 - Formação ASP.Net Core/02 - .Net 6 - Relacionando Entidades/FilmesApi/FilmesApi/Data/FilmeContext.cs
@@ -0,0 +1,12 @@
+using FilmesApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmesApi.Data;
+
+public class FilmeContext: DbContext{
+    public DbSet<Filme> Filmes { get; set; }
+
+    public FilmeContext(DbContextOptions<FilmeContext> options) : base(options){ }
+
+
+}
