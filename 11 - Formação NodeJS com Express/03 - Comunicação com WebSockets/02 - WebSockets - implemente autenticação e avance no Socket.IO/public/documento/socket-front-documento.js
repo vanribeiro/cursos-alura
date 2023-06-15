b223230852aa028bf5fdd81eb816d4bc521cec85
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2023-06-15 00:00:10 -0300

    aula 04 - parte 01

--- 11 - Formação NodeJS com Express/03 - Comunicação com WebSockets/02 - WebSockets - implemente autenticação e avance no Socket.IO/public/documento/socket-front-documento.js
+++ 11 - Formação NodeJS com Express/03 - Comunicação com WebSockets/02 - WebSockets - implemente autenticação e avance no Socket.IO/public/documento/socket-front-documento.js
@@ -1,17 +1,23 @@
 import { obterCookie } from "../utils/cookies.js";
-import { alertarERedirecionar, atualizaTextoEditor } from "./documento.js";
+import { 
+  alertarERedirecionar,
+  atualizaTextoEditor,
+  tratarAutorizacaoSucesso 
+} from "./documento.js";
 
 const socket = io('/usuarios', {
   auth: { token: obterCookie('tokenJwt')  }
 });
+
+socket.on("autorizacao_sucesso", tratarAutorizacaoSucesso);
 
 socket.on("connect_error", (erro) => {
   alert(erro);
   window.location.href = '/login/index.html';
 });
 
-function selecionarDocumento(nome) {
-  socket.emit("selecionar_documento", nome, (texto) => {
+function selecionarDocumento(dadosEntrada) {
+  socket.emit("selecionar_documento", dadosEntrada, (texto) => {
     atualizaTextoEditor(texto);
   });
 }
