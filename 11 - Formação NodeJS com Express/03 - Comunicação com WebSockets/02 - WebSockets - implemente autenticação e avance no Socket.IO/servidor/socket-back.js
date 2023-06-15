f9f6c7383d1c9b4caa202bbcc3535697460aa243
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2023-06-14 23:40:49 -0300

    Aula 03

--- 11 - Formação NodeJS com Express/03 - Comunicação com WebSockets/02 - WebSockets - implemente autenticação e avance no Socket.IO/servidor/socket-back.js
+++ 11 - Formação NodeJS com Express/03 - Comunicação com WebSockets/02 - WebSockets - implemente autenticação e avance no Socket.IO/servidor/socket-back.js
@@ -1,13 +1,23 @@
 import 'dotenv/config';
-import { registrarEventosCadastro } from "./registrarEventos/cadastro.js";
-import { registrarEventosDocumento } from "./registrarEventos/documento.js";
-import { registrarEventosInicio } from "./registrarEventos/inicio.js";
-import { registrarEventosLogin } from "./registrarEventos/login.js";
-import io from "./servidor.js";
+import { registrarEventosCadastro } from './registrarEventos/cadastro.js';
+import { registrarEventosDocumento } from './registrarEventos/documento.js';
+import { registrarEventosInicio } from './registrarEventos/inicio.js';
+import { registrarEventosLogin } from './registrarEventos/login.js';
+import io from './servidor.js';
+import { autorizarUsuario } from './middlewares/autorizarUsuario.js';
 
-io.on("connection", (socket) => {
+// nsp = namespace
+
+const nspUsuarios = io.of('/usuarios');
+
+nspUsuarios.use(autorizarUsuario);
+
+nspUsuarios.on('connection', (socket) => {
+  registrarEventosDocumento(socket, nspUsuarios);
+  registrarEventosInicio(socket, nspUsuarios);
+});
+
+io.of('/').on('connection', (socket) => {
   registrarEventosCadastro(socket, io);
-  registrarEventosDocumento(socket, io);
   registrarEventosLogin(socket, io);
-  registrarEventosInicio(socket, io);
 });
