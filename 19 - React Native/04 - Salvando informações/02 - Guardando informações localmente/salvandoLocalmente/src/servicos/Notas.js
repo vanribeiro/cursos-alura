ccaf5afd93e4c291dfdb61581640ae88093e39ea
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2023-06-26 16:09:37 -0300

    [salvando dados localmente]: aulas 04 - parte 02 e desafio finalizado

--- 19 - React Native/04 - Salvando informações/02 - Guardando informações localmente/salvandoLocalmente/src/servicos/Notas.js
+++ 19 - React Native/04 - Salvando informações/02 - Guardando informações localmente/salvandoLocalmente/src/servicos/Notas.js
@@ -10,18 +10,6 @@ function criarTabela() {
     })
 }
 
-async function atualizarNota({ titulo, categoria, texto, id }) {
-    return new Promise((resolve, reject) => {
-        bancoDeDados.transaction((transaction) => {
-            transaction.executeSql(
-            'UPDATE Notas SET titulo = ?, categoria = ?, texto = ? WHERE id = ?' + 
-            [titulo, categoria, texto, id],
-            () => resolve('Nota atualizada com sucesso!'),
-            () => reject('Erro ao tentar adicionar uma nota...'));
-        });
-    });
-}
-
 async function adicionarNota({ titulo, categoria, texto }) {
     return new Promise((resolve, reject) => {
         bancoDeDados.transaction((transaction) => {
@@ -30,6 +18,29 @@ async function adicionarNota({ titulo, categoria, texto }) {
             'VALUES (?, ?, ?);', [titulo, categoria, texto],
             () => resolve('Nota adicionada com sucesso!'),
             () => reject('Erro ao tentar adicionar uma nota...'));
+        });
+    });
+}
+
+async function atualizarNota({ titulo, categoria, texto, id }) {
+    return new Promise((resolve, reject) => {
+        bancoDeDados.transaction((transaction) => {
+            transaction.executeSql(
+            'UPDATE Notas SET titulo = ?, categoria = ?, texto = ? WHERE id = ?;', 
+            [titulo, categoria, texto, id],
+            () => resolve('Nota atualizada com sucesso!'),
+            () => reject('Erro ao tentar atualizar nota...'));
+        });
+    });
+}
+
+async function removerNota({id}) {
+    return new Promise((resolve, reject) => {
+        bancoDeDados.transaction((transaction) => {
+            transaction.executeSql(
+            'DELETE FROM Notas WHERE id = ?;', [id],
+            () => resolve('Nota removida!'),
+            () => reject('Erro ao tentar remover nota...'));
         });
     });
 }
@@ -45,9 +56,22 @@ async function buscarNotas() {
     });
 }
 
+async function buscarNotasPorCategoria({categoria}) {
+    return new Promise((resolve, reject) => {
+        bancoDeDados.transaction((transaction) => {
+            transaction.executeSql(
+            'SELECT * FROM Notas WHERE categoria = ?;', [categoria],
+            (___, resultado) => resolve(resultado.rows._array),
+            () => reject('Erro ao tentar buscar notas...'));
+        });
+    });
+}
+
 export {
     criarTabela,
     adicionarNota,
     buscarNotas,
-    atualizarNota
+    atualizarNota,
+    removerNota,
+    buscarNotasPorCategoria
 }
