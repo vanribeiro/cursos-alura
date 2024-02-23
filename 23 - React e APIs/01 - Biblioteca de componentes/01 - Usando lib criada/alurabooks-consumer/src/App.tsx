4e63e935eb90875297ef0b52f62800c64f529960
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2024-02-23 17:04:11 -0300

    curso finalizado

---
+++ 23 - React e APIs/01 - Biblioteca de componentes/01 - Usando lib criada/alurabooks-consumer/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { AbBotao } from 'vcr-comp-lib';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <AbBotao />
+      </header>
+    </div>
+  );
+}
+
+export default App;
