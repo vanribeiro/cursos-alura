3df6ba62f1a14b881eba45835438009c9436cc01
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2024-03-05 18:28:24 -0300

    aula 03 - finalizada

--- 24 - Aleatórios/01 - React com Vite/codeconnect/src/components/Main/index.tsx
+++ 24 - Aleatórios/01 - React com Vite/codeconnect/src/components/Main/index.tsx
@@ -1,11 +1,13 @@
 import Filter from '../Filter';
 import SearchBar from '../SearchBar';
+import Sorting from '../Sorting';
 
 function Main() {
     return(
         <main>
             <SearchBar />
             <Filter />
+            <Sorting />
         </main>
     );
 }
