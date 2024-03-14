aee7e8af6fce29e381d2ec65ecae1c15155ccd51
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2024-03-14 00:08:59 -0300

    continuação página

--- 23 - React e APIs/02 - Autenticação/01 - Autenticação de Usuários/alura-books-vr/src/components/Header/Logo/index.tsx
+++ 23 - React e APIs/02 - Autenticação/01 - Autenticação de Usuários/alura-books-vr/src/components/Header/Logo/index.tsx
@@ -1,14 +1,37 @@
 import styled from "styled-components";
-
+import LogoIcone from './../assets/logo-icone.svg';
+import LogoLetras from './../assets/logo-letras.svg';
 
 const LogoContainer = styled.figure`
-    
+    margin: 0;
+    padding: 0;
+    margin-right: 24px;
+
+    @media screen and (min-width: 1024px){
+        padding-left: 80px;
+    }
+
 `;
+
+const Img = styled.img`
+    padding: 0 4px;
+`;
+
+const Letters = styled(Img)`
+
+    @media screen and (max-width: 428px){
+        display: none;
+    }
+`;
+
 
 function Logo() {
     return (
         <LogoContainer>
-
+            <a href="/">
+                <Img src={LogoIcone} alt="Ícone do Logo" />
+                <Letters src={LogoLetras} alt="Letras do Logo" />
+            </a>
         </LogoContainer>
     );
 }
