ec19180d0c6266466495d7593e6bbb3cc2f61bce
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2024-03-15 15:12:54 -0300

    header finalizado

--- 23 - React e APIs/02 - Autenticação/01 - Autenticação de Usuários/alura-books-vr/src/components/Header/Logo/index.tsx
+++ 23 - React e APIs/02 - Autenticação/01 - Autenticação de Usuários/alura-books-vr/src/components/Header/Logo/index.tsx
@@ -1,9 +1,6 @@
 import styled from "styled-components";
 import LogoIcone from './../assets/logo-icone.svg';
 import LogoLetras from './../assets/logo-letras.svg';
-import HamburgerBlueIcon from './../assets/hambuger-blue.svg';
-import HamburgerWhiteIcon from './../assets/hamburger-white.svg';
-import { useState } from "react";
 
 const LogoContainer = styled.figure`
     margin: 0;
@@ -11,69 +8,37 @@ const LogoContainer = styled.figure`
     display: flex;
     align-items: center;
     
-    @media screen and (min-width: 1024px){
+    @media screen and (min-width: 769px){
         padding-left: 80px;
         margin-right: 24px;
+
+        a{
+            display: flex;
+            margin-right: 8px;
+        }
     }
 
 `;
 
 const Img = styled.img`
     padding: 0 4px;
+
+    @media screen and (max-width: 767.99px){
+        margin-left: 8px;
+    }
 `;
 
 const Letters = styled(Img)`
 
-    @media screen and (max-width: 428px){
+    @media screen and (max-width: 767.99px){
         display: none;
     }
 `;
 
-const MenuButton = styled.button`
-    background-color: white;
-    border: none;
-    height: 90px;
-    width: 80px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    
-    &:active{
-        cursor: pointer;
-        color: #FFFFFF;
-        background: linear-gradient(97.54deg, #002F52 35.49%, #326589 165.37%);
-        text-decoration: none;
-    }
-
-    @media screen and (min-width: 428px){
-        display: none;
-
-    }
-
-`
-
 function Logo() {
-    const [menuIcon, setMenuIcon] = useState<string>(HamburgerBlueIcon);
-    
-    const handleClick = () => {
-        menuIcon === HamburgerBlueIcon ?
-        setMenuIcon(HamburgerWhiteIcon):
-        setMenuIcon(HamburgerBlueIcon);
-    }
 
     return (
 		<LogoContainer>
-			<MenuButton
-				type="button"
-				onClick={() => handleClick()}>
-				<Img
-					height={48}
-					width={48}
-					src={menuIcon}
-					alt="Ícone do Menu"
-				/>
-			</MenuButton>
 			<a href="/">
 				<Img
 					src={LogoIcone}
--- 23 - React e APIs/02 - Autenticação/01 - Autenticação de Usuários/alura-books-vr/src/components/Header/index.tsx
+++ 23 - React e APIs/02 - Autenticação/01 - Autenticação de Usuários/alura-books-vr/src/components/Header/index.tsx
@@ -2,19 +2,27 @@ import styled from 'styled-components';
 import Menu from "./Menu";
 import Logo from './Logo';
 import Action from './Action';
-// import { useState } from 'react';
+import MenuMobile from './Menu/MenuMobile';
 
 const HeaderContainer = styled.header`
   height: 90px;
   display: flex;
   justify-content: center;
   align-items: center;
+
+  @media screen and (min-width: 769px) {
+    
+    .menu-mobile{
+        display: none;
+    }
+  }
 `;
 
 function Header() {
-    // const [isMenuMobileOpen, setIsMenuMobileOpen] = useState<boolean>(false);
+
     return (
         <HeaderContainer>
+            <MenuMobile />
             <Logo />
             <Menu />
             <Action />
