f9ca4939c02699983926cffc5925ff5251bd01a9
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2025-02-01 21:00:58 -0300

    aula 02

--- 26 - UI Design para Devs/02 - Heuríticas/code/js/scripts.js
+++ 26 - UI Design para Devs/02 - Heuríticas/code/js/scripts.js
@@ -1,15 +1,8 @@
-
 import { carregarItensDaSacola } from "./carregarItensDaSacola.js";
 import { imprimirCamisetas } from "./imprimirCamisetas.js";
 import { imprimirUmDeCadaCategoria } from "./imprimirUmDeCadaCategoria.js";
 import { getItemsFromLocalStorage } from "./localstorage.js";
 
-
-// Chama a função para carregar os itens da sacola ao carregar a página
-
-
-
-// Função para verificar se a página atual é /camisetas.html
 function isPaginaCamisetas() {
   return window.location.pathname.includes("/camisetas.html");
 }
@@ -22,36 +15,65 @@ if (isPaginaSacola()) {
   carregarItensDaSacola();
 }
 
-// Função para imprimir somente as camisetas
 function separarCamisetas(produtos) {
   const camisetas = produtos.produtos.camisetas;
 
-  // Verifica se a página atual é /camisetas.html
   if (isPaginaCamisetas()) {
     imprimirCamisetas(camisetas);
   }
 }
 
-fetch("./json/produtos.json")
-  .then((response) => response.json())
-  .then((produtosJSON) => {
-    if (isPaginaCamisetas()) {
-      separarCamisetas(produtosJSON);
-    } 
-    
-    imprimirUmDeCadaCategoria(produtosJSON);
-  })
-  .catch((error) => console.error("Erro ao carregar o arquivo JSON:", error));
+const products = document.getElementById('produtos');
+
+const loadProducts = (localization = 'BR') => {
+  if (products) {
+    products.innerHTML = ''
+  }
+
+  let url = "./json/produtos.json";
+
+  if(localization === 'EUA') {
+    url = "./json/products.json";
+  }
+
+  fetch(url)
+    .then(response => response.json())
+    .then((produtosJSON) => {
+      if (isPaginaCamisetas()) {
+        separarCamisetas(produtosJSON);
+      }
+      imprimirUmDeCadaCategoria(produtosJSON);
+    })
+    .catch((error) => {
+      console.error("Erro ao carregar o arquivo JSON:", error);
+    });
+
+}
+
+loadProducts();
+
+const allLanguagesToTranslate = document.querySelectorAll('[data-language-to-translate]');
+
+if(allLanguagesToTranslate) {
+  allLanguagesToTranslate.forEach(language => 
+    language.addEventListener('click', () => {
+      const selectedLanguage = language.dataset.languageToTranslate;
+      loadProducts(selectedLanguage);
+    })
+  );
+}
+
 
 const bagLink = document.querySelector('.bag-link');
 const bagBadgeCounter = document.querySelector('.bag-items-counter');
 
 if(bagLink) {
   const items = getItemsFromLocalStorage('sacola');
-  const itemsQuantity = items.length || 0;
-
-  if(bagBadgeCounter) {
-    bagBadgeCounter.innerText = itemsQuantity;
+  if(items) {
+    const itemsQuantity = items.length || 0;
+  
+    if(bagBadgeCounter) {
+      bagBadgeCounter.innerText = itemsQuantity;
+    }
   }
-  // console.log()
 }
